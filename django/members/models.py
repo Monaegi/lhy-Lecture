3097bea4257a7aaecacc9fb01868ebb6a09847ad
LeeHanYeong <dev@azelf.com>
2018-01-19 19:50:51 +0900

    User __str__수정

--- django/members/models.py
+++ django/members/models.py
@@ -1,27 +1,39 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from utils.models import TimeStampedMixin
+
 
 class User(AbstractUser):
-    PERIOD_CHOICES = [(f'{x}', f'{x}기') for x in range(3, 8)]
-    USER_TYPE_CHOICES = (
-        ('instructor', '강사'),
-        ('assistant', '조교'),
-        ('manager', '매니저'),
-        ('student', '수강생'),
-    )
-
     email = models.EmailField('이메일')
     name = models.CharField('이름', max_length=20)
-    period = models.CharField('기수', max_length=2, choices=PERIOD_CHOICES, blank=True)
-    user_type = models.CharField('사용자 타입', max_length=20, choices=USER_TYPE_CHOICES)
     img_profile = models.ImageField('프로필 이미지', upload_to='user', blank=True)
+    courses = models.ManyToManyField(
+        'course.Course',
+        through='Membership',
+        related_name='users',
+        related_query_name='user',
+        blank=True
+    )
 
     # disable exist fields
     first_name = None
     last_name = None
 
     def __str__(self):
-        if self.period:
-            return f'{self.get_period_display()} | {self.name}'
-        return f'{self.get_user_type_display()} | {self.name}'
+        return f'{self.name}'
+
+
+class Membership(TimeStampedMixin, models.Model):
+    MEMBERSHIP_TYPE_CHOICES = (
+        ('instructor', '강사'),
+        ('assistant', '조교'),
+        ('manager', '매니저'),
+        ('student', '수강생'),
+    )
+    user = models.ForeignKey(User, verbose_name='사용자')
+    course = models.ForeignKey('course.Course', verbose_name='해당 코스')
+    type = models.CharField('사용자 타입', max_length=20, choices=MEMBERSHIP_TYPE_CHOICES)
+
+    def __str__(self):
+        return f'{self.course.subject.title}'
