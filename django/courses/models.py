b4405201c5f93a4b840bc45a65712d05a33324f7
LeeHanYeong <dev@azelf.com>
2018-01-23 21:12:48 +0900

    verbose_name설정

--- django/courses/models.py
+++ django/courses/models.py
@@ -4,6 +4,10 @@ from django.db import models
 class Subject(models.Model):
     title = models.CharField(max_length=200)
 
+    class Meta:
+        verbose_name = '강의'
+        verbose_name_plural = f'{verbose_name} 목록'
+
     def __str__(self):
         return self.title
 
@@ -11,6 +15,10 @@ class Subject(models.Model):
 class Course(models.Model):
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
     period = models.IntegerField(default=1)
+
+    class Meta:
+        verbose_name = '강의 기수'
+        verbose_name_plural = f'{verbose_name} 목록'
 
     def __str__(self):
         return f'{self.subject.title} ({self.period}기)'
